41eaf9e49efdee6fa3c9913e5124f02ac3eeb11a
BAMBOO4-design <golden.bamboo4444@gmail.com>
2025-10-19 21:40:38 +0800

    rename and move files into dir

--- main.ts
+++ main.ts
@@ -1,134 +1,160 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, App, Setting, PluginSettingTab } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ImageManagerSettings {
+  isEnabled: boolean;
+  targetDirectory: string;
+  imagePrefix: string;
+  targetNote: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ImageManagerSettings = {
+  isEnabled: true,
+  targetDirectory: '',
+  imagePrefix: 'prefix',
+  targetNote: '',
+};
+
+export default class ImageManagerPlugin extends Plugin {
+  settings: ImageManagerSettings;
+
+  constructor(app: App, manifest: any) {
+    super(app, manifest);
+    this.settings = DEFAULT_SETTINGS;
+  }
+
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new ImageManagerSettingTab(this.app, this));
+    this.addCommand({
+      id: 'manage-image-attachments',
+      name: 'Manage Image Attachments',
+      callback: () => this.manageImageAttachments(),
+    });
+    console.log("Plugin Loaded");
+  }
+
+  async manageImageAttachments() {
+    if (!this.settings.isEnabled) return;
+
+    const targetNote = await this.getTargetNote();
+    if (!targetNote) return;
+
+    const images = await this.getImageAttachments(targetNote);
+    if (images.length === 0) return;
+
+    const targetFolder = this.settings.targetDirectory || this.app.vault.getRoot().path;
+    await this.ensureTargetFolderExists(targetFolder);
+
+    let updatedContent = await this.app.vault.read(targetNote);
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i];
+      const newFileName = this.renameFile(image, i + 1);
+      updatedContent = updatedContent.replace(`![[${image}]]`, `![[${newFileName}]]`);
+      await this.moveFile(image, newFileName, targetFolder);
+    }
+
+    await this.app.vault.modify(targetNote, updatedContent);
+    console.log("Image management complete.");
+  }
+
+  async getTargetNote(): Promise<any> {
+    const targetNoteName = this.settings.targetNote;
+    if (!targetNoteName) return null;
+
+    const notePath = targetNoteName.endsWith('.md') ? targetNoteName : `${targetNoteName}.md`;
+    return this.app.vault.getFiles().find(file => file.path === notePath);
+  }
+
+  async getImageAttachments(note: any): Promise<string[]> {
+    const content = await this.app.vault.read(note);
+    const images: string[] = [];
+    const regex = /!\[\[([^\]]+)\]\]/g;
+    let match;
+    while ((match = regex.exec(content)) !== null) images.push(match[1]);
+    return images;
+  }
+
+  async ensureTargetFolderExists(folderPath: string): Promise<void> {
+    const targetFolder = this.app.vault.getAbstractFileByPath(folderPath);
+    if (!targetFolder) await this.app.vault.createFolder(folderPath);
+  }
+
+  renameFile(filePath: string, index: number): string {
+    const fileName = filePath.split('/').pop();
+    const fileExtension = fileName?.split('.').pop();
+    return fileName && fileExtension ? `${this.settings.imagePrefix}${index}.${fileExtension}` : `${this.settings.imagePrefix}${index}.unknown`;
+  }
+
+  async moveFile(filePath: string, newFileName: string, targetFolder: string): Promise<void> {
+    const file = this.app.vault.getAbstractFileByPath(filePath);
+    if (!file) return;
+
+    const newFilePath = `${targetFolder}/${newFileName}`;
+    const existingFile = this.app.vault.getAbstractFileByPath(newFilePath);
+    if (existingFile) await this.app.vault.delete(existingFile);
+
+    await this.app.vault.rename(file, newFilePath);
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  onunload() {
+    console.log("Image Manager Plugin Unloaded");
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class ImageManagerSettingTab extends PluginSettingTab {
+  plugin: ImageManagerPlugin;
 
-	async onload() {
-		await this.loadSettings();
+  constructor(app: App, plugin: ImageManagerPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    new Setting(containerEl)
+      .setName('Enable/Disable Plugin')
+      .addToggle(toggle =>
+        toggle.setValue(this.plugin.settings.isEnabled).onChange(async (value) => {
+          this.plugin.settings.isEnabled = value;
+          await this.plugin.saveSettings();
+        })
+      );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    new Setting(containerEl)
+      .setName('Target Directory')
+      .addText(text =>
+        text.setValue(this.plugin.settings.targetDirectory).onChange(async (value) => {
+          this.plugin.settings.targetDirectory = value;
+          await this.plugin.saveSettings();
+        })
+      );
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    new Setting(containerEl)
+      .setName('Image Renaming Prefix')
+      .addText(text =>
+        text.setValue(this.plugin.settings.imagePrefix).onChange(async (value) => {
+          this.plugin.settings.imagePrefix = value;
+          await this.plugin.saveSettings();
+        })
+      );
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Target Note')
+      .addText(text =>
+        text.setValue(this.plugin.settings.targetNote).onChange(async (value) => {
+          this.plugin.settings.targetNote = value;
+          await this.plugin.saveSettings();
+        })
+      );
+  }
 }
